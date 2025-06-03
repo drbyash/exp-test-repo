8181fd6679cf901aff1aae61f941283bf3f26c33
drbyash <drbyash@amazon.com>
2025-06-03 21:46:50 +0530

    Update opensearch_access_control.tf

--- abstraction/opensearch_access_control.tf
+++ abstraction/opensearch_access_control.tf
@@ -39,6 +39,17 @@ default     = null
  sensitive   = true
 }
 
+variable "user_password" {
+  description = "Enter the user password (required if creating user)"
+  type        = string
+  default     = null
+
+  validation {
+    condition     = var.user_password == null || length(var.user_password) >= 8
+    error_message = "Password must be at least 8 characters long when provided."
+  }
+}
+
 variable "aws_region" {
   description = "AWS region where the OpenSearch cluster is deployed"
   type        = string
