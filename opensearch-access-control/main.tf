0e36bbd62ea3f649d6389d361a586673864b2001
drbyash <drbyash@amazon.com>
2025-06-03 14:33:01 +0530

    Update main.tf

--- opensearch-access-control/main.tf
+++ opensearch-access-control/main.tf
@@ -1,15 +1,15 @@
 module "opensearch_users" {
-  source = "./opensearch-access-control/modules/opensearch_user"
+  source = "modules/opensearch_user"
   users  = var.opensearch_users
 }
 
 module "opensearch_roles" {
-  source = "./opensearch-access-control/modules/opensearch_role"
+  source = "modules/opensearch_role"
   roles  = var.opensearch_roles
 }
 
 module "opensearch_role_mappings" {
-  source        = "./opensearch-access-control/modules/opensearch_role_mapping"
+  source        = "modules/opensearch_role_mapping"
   role_mappings = var.role_mappings
 }
 
