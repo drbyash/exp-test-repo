4adee4337f9de20e17e0d64399f68da0aeb0bb87
drbyash <drbyash@amazon.com>
2025-06-05 12:51:10 +0530

    Update main.tf

--- abstraction/opensearch-project/modules/opensearch_access_control/main.tf
+++ abstraction/opensearch-project/modules/opensearch_access_control/main.tf
@@ -1,10 +1,45 @@
-# modules/opensearch_access_control/main.tf
+# Local variables for resource creation
+locals {
+  # User configurations
+  users = {
+    for role_name, config in var.application_config.access_control :
+    "${var.application_config.name}_${role_name}_user" => {
+      password = config.password
+      # backend_roles = config.backend_roles 
+      attributes = merge({ #standard attributes
+        application = var.application_config.name
+        role = role_name
+      },
+      config.custom_attributes   #user-defined attributes
+      )
+    }
+    if config.create_user
+  }
+
+  # Role configurations
+  roles = {
+    for role_name, config in var.application_config.access_control :
+    "${var.application_config.name}_${role_name}" => config.permissions
+    if config.create_role
+  }
+
+  # Role mapping configurations
+  role_mappings = merge([
+    for role_name, config in var.application_config.access_control : {
+      "${config.create_role ? "${var.application_config.name}_${role_name}" : config.existing_roles}" = {
+        backend_roles = config.backend_roles
+        users = config.create_user ? ["${var.application_config.name}_${role_name}_user"] : []
+      }
+    }
+  ]...)
+}
+
 resource "opensearch_user" "users" {
   for_each = local.users
 
   username      = each.key
   password      = each.value.password
-  backend_roles = each.value.backend_roles
+  # backend_roles = each.value.backend_roles
   attributes    = each.value.attributes
 }
 
@@ -39,39 +74,8 @@ resource "opensearch_roles_mapping" "mappings" {
   backend_roles = each.value.backend_roles
   users         = each.value.users
 
+
+
+
   depends_on = [opensearch_role.roles, opensearch_user.users]
-}
-
-# Local variables for resource creation
-locals {
-  # User configurations
-  users = {
-    for role_name, config in var.application_config.access_control :
-    "${var.application_config.name}_${role_name}_user" => {
-      password = config.password
-      backend_roles = []
-      attributes = {
-        application = var.application_config.name
-        role = role_name
-      }
-    }
-    if config.create_user
-  }
-
-  # Role configurations
-  roles = {
-    for role_name, config in var.application_config.access_control :
-    "${var.application_config.name}_${role_name}" => config.permissions
-    if config.create_role
-  }
-
-  # Role mapping configurations
-  role_mappings = merge([
-    for role_name, config in var.application_config.access_control : {
-      "${config.create_role ? "${var.application_config.name}_${role_name}" : config.existing_role}" = {
-        backend_roles = config.backend_roles
-        users = config.create_user ? ["${var.application_config.name}_${role_name}_user"] : []
-      }
-    }
-  ]...)
 }
