b90028b01de7bf30e06ba31ca445ae4d0f04cd5e
drbyash <drbyash@amazon.com>
2025-06-05 12:51:35 +0530

    Update outputs.tf

--- abstraction/opensearch-project/modules/opensearch_access_control/outputs.tf
+++ abstraction/opensearch-project/modules/opensearch_access_control/outputs.tf
@@ -1,15 +1,11 @@
 # modules/opensearch_access_control/outputs.tf
 output "users" {
-  description = "Created OpenSearch users"
+  description = "OpenSearch users"
   value = [for user in opensearch_user.users : user.username]
 }
 
 output "roles" {
-  description = "Created OpenSearch roles"
+  description = "OpenSearch roles"
   value = [for role in opensearch_role.roles : role.role_name]
 }
 
-output "role_mappings" {
-  description = "Created OpenSearch role mappings"
-  value = [for mapping in opensearch_roles_mapping.mappings : mapping.role_name]
-}
